f2ee4c52e884cfb9390d59af8dfa4f54d7bad062
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2026-07-25 17:21:02 -0500

    chore: adjust logger

--- fintech-api/src/config/logger.ts
+++ fintech-api/src/config/logger.ts
@@ -1,19 +1,36 @@
 import winston from 'winston';
 
+const { combine, timestamp, errors, printf, json, colorize } = winston.format;
+
+const prettyFormat = printf(({ level, message, timestamp, stack, ...meta }) => {
+  return JSON.stringify(
+    {
+      timestamp,
+      level,
+      message: stack || message,
+      ...meta,
+    },
+    null,
+    2
+  );
+});
+
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.errors({ stack: true }),
-    winston.format.json()
-  ),
+  format: combine(timestamp(), errors({ stack: true })),
   transports: [],
 });
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: combine(colorize(), prettyFormat),
+    })
+  );
+} else {
+  logger.add(
+    new winston.transports.Console({
+      format: combine(timestamp(), errors({ stack: true }), json()),
     })
   );
 }
